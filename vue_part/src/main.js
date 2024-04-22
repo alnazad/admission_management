ae993a0f34435438971b57673156ac80945411aa
Abdullah <abdullahalnazad850@gmail.com>
2024-04-22 20:57:24 +0600

    routing vue

--- vue_part/src/main.js
+++ vue_part/src/main.js
@@ -1,11 +1,20 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import HomeView from './views/HomeView.vue'
+import AboutView from './views/AboutView.vue'
+import CircularView from './views/CircularView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+const routes=[
+    {path:'/', component: HomeView},
+    {path:'/about', component: AboutView},
+    {path:'/circulation', component: CircularView},
+];
+const router=createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
 
-const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
--- vue_part/src/views/HomeView.vue
+++ vue_part/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>This Is Home Page</h1>
 </template>
